782605393c30bc04c6c82f1360d7fca900d6d313
Merden_Hran <merdenhran@gmail.com>
2023-03-19 11:34:02 +0200

    V2 working logic

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     [SerializeField] private bool _startGame = false;
+    [SerializeField] private PlatformSpawner spawner;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        spawner.StartSpawn();
     }
 
     // Update is called once per frame
